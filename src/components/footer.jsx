8e4812d04efb67b75f1fb67b319f3f674ad7a177
tylersmith <tyjsmith1@gmail.com>
2024-09-11 17:49:37 -0500

    firebase init

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-            <p className="text-xs text-gray-500 dark:text-gray-400">© 2023 Acme Inc. All rights reserved.</p>
+            <p className="text-xs text-gray-500 dark:text-gray-400">This is a demo app.</p>
             <nav className="sm:ml-auto flex gap-4 sm:gap-6">
                 <a className="text-xs hover:underline underline-offset-4" href="#">
                     Terms of Service
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 export default function Hero() {
     return (
         <section className="relative w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-cover bg-center">
+            <div className="absolute inset-0 bg-black opacity-10 z-10"></div>
             <Image
                 src="/assets/closet1.jpg"
                 alt="Background Image"
@@ -15,7 +16,7 @@ export default function Hero() {
             <div className="container mx-auto px-4 md:px-6 flex justify-center items-center h-full relative z-10">
                 <div className="flex flex-col items-center space-y-4 text-center">
                     <div className="space-y-2">
-                        <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none text-white">
+                        <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none gradient-text">
                             Welcome to Cozy Threads
                         </h1>
                         <p className="mx-auto max-w-[700px] text-gray-200 md:text-xl">
